1c8eb791b688a215349235f9b323af5f06010541
J.Luis <root@heavydeck.net>
2019-12-30 00:33:03 +0100

    Added 'Save As...' plugin too.

--- make_kicad.py
+++ make_kicad.py
@@ -5,9 +5,9 @@ class KicadExports(inkex.Effect):
     def __init__(self):
         inkex.Effect.__init__(self)
         #Argument parser
-        self.OptionParser.add_option('--layer',  action='store', type='string', dest='layer',  default='AUTO',      help='Layer to use')
-        self.OptionParser.add_option('--name',   action='store', type='string', dest='name',   default='Footprint', help='Footprint name')
-        self.OptionParser.add_option('--output', action='store', type='string', dest='output', default=None,        help='Output file')
+        self.OptionParser.add_option('--layer',  action='store', type='string', dest='layer',  default='AUTO', help='Layer to use')
+        self.OptionParser.add_option('--name',   action='store', type='string', dest='name',   default=None,   help='Footprint name')
+        self.OptionParser.add_option('--output', action='store', type='string', dest='output', default=None,   help='Output file')
 
     def affect(self):
         (self.options, self.args) = self.OptionParser.parse_args()
