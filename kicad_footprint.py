0df04b4caa3a8f7bdb2ffed8796c26fd6a019f64
J.Luis <root@heavydeck.net>
2020-01-05 17:20:23 +0100

    Added licensing, Strict GPLv2.

--- kicad_footprint.py
+++ kicad_footprint.py
@@ -1,3 +1,17 @@
+'''
+Inkscape to KiCAD footprint plugin.
+Copyright (C) 2020  J.Luis Álvarez
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+version 2 as published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+'''
+
 import inkex
 import cspsubdiv, cubicsuperpath, simpletransform
 import sys
